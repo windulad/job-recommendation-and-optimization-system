270ee7abeed113f9ae13ab52a10b3134e37fb817
Windula Dissanayake <charithwinwindula@gmail.com>
2023-01-26 05:10:01 +0530

    commit db

--- develop-back-end/main-system/app.py
+++ develop-back-end/main-system/app.py
@@ -1,6 +1,30 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__,template_folder = 'template')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5433/accounts'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Account(db.Model):
+    __tablename__ = 'accounts'
+
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(200), nullable = False)
+    username = db.Column(db.String(50), nullable = False)
+    password = db.Column(db.String(20), nullable = False)
+    confirm_password = db.Column(db.String(20), nullable = False)
+
+    def __init__(id, email, password, confirm_password):
+        self.id = id
+        self.email = email
+        self.username = username
+        self.password = password
+        self.confirm_password = confirm_password
+    
+    def __repr__(self):
+        return f"<Account {self.email}>"
 
 # Main route of the application
 @app.route('/')
