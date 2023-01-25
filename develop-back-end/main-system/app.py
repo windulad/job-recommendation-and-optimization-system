54e3abd33cccf03087b9e7079c6e8f33a112d95c
Windula Dissanayake <charithwinwindula@gmail.com>
2023-01-26 03:02:19 +0530

    commit new files

--- develop-back-end/main-system/app.py
+++ develop-back-end/main-system/app.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__,template_folder = 'template')
 
+# Main route of the application
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# Route to create an account page
+@app.route('/createacc.html')
+def createacc():
+    return render_template('createacc.html')
+
+# Route to login page
+@app.route('/login.html')
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
