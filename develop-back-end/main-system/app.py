a7fc2b355b259395b91d77761fa14f8d39bc2c21
Windula Dissanayake <charithwinwindula@gmail.com>
2023-02-06 01:16:38 +0530

    commit new files

--- develop-back-end/main-system/app.py
+++ develop-back-end/main-system/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__,template_folder = 'template')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5433/accounts'
 # db = SQLAlchemy(app)
 
-# Connect to 'flask_db' database
+# Connect to 'flask_db' database------------------------
 conn = psycopg2.connect(
         database="flask_db",
         user="postgres",
@@ -14,10 +14,8 @@ conn = psycopg2.connect(
         host="localhost",
         port="5433"
         )
-
 # create a cursor to perform database operations
 cur = conn.cursor()
-
 # Create 'userdata' table
 cur.execute('CREATE TABLE IF NOT EXISTS userdata ('
                 'id serial PRIMARY KEY,'
@@ -25,7 +23,6 @@ cur.execute('CREATE TABLE IF NOT EXISTS userdata ('
                 'username varchar (50) NOT NULL,'
                 'password varchar (50) NOT NULL);'
                 )
-  
 # Insert some data into the 'userdata' table
 cur.execute('INSERT INTO userdata (email, username, password)'
                 'VALUES (%s, %s, %s)',
@@ -33,19 +30,63 @@ cur.execute('INSERT INTO userdata (email, username, password)'
                 'username1',
                 'userpassword1')
                 )
-  
 # commit the changes
 conn.commit()
-  
 # close the cursor and connection
 cur.close()
 conn.close()
 
-# Create an account
-@app.route('/createacc', methods=['POST'])
+# Create an account-------------------------------------
+@app.route('/createnewacc', methods=['POST'])
+def createnewacc():
+    # Connect to 'flask_db' database
+    conn = psycopg2.connect(
+            database="flask_db",
+            user="postgres",
+            password="admin",
+            host="localhost",
+            port="5433"
+            )
+    # create a cursor to perform database operations
+    cur = conn.cursor()
+    # Get the data from the form
+    email = request.form['email']
+    username = request.form['username']
+    password = request.form['password']
+    # Insert the data into the table
+    cur.execute('INSERT INTO userdata (email, username, password)'
+                'VALUES (%s, %s, %s)',
+                (email, username, password)
+                )
+    # commit the changes
+    conn.commit()
+    # close the cursor and connection
+    cur.close()
+    conn.close()
+    return render_template('enterskills.html')
 
-# Log in
-@app.route('/login', methods=['POST'])
+# Log in---------------------------------------------
+@app.route('/loginacc', methods=['POST'])
+def loginacc():
+    # Connect to 'flask_db' database
+    conn = psycopg2.connect(
+            database="flask_db",
+            user="postgres",
+            password="admin",
+            host="localhost",
+            port="5433"
+            )
+    # create a cursor to perform database operations
+    cur = conn.cursor()
+    # Get the data from the form
+    email = request.form['email']
+    password = request.form['password']
+    # 
+
+    # close the cursor and connection
+    cur.close()
+    conn.close()
+    return render_template('enterskills.html')
 
 # Main route of the application
 @app.route('/')
