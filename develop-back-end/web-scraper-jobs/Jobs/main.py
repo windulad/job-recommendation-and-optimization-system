d20e7e7ae96c93f47aca2c57a8186e289bc11d2d
wijethucs19028 <120020452+wijethucs19028@users.noreply.github.com>
2023-05-15 15:15:24 +0530

    commit

--- develop-back-end/web-scraper-jobs/Jobs/main.py
+++ develop-back-end/web-scraper-jobs/Jobs/main.py
@@ -1,22 +1,45 @@
 from bs4 import BeautifulSoup
 import requests
-#chira
-print("Put Some Skill That You Are Not Familiar With ")
-unfamilliar_skill=input('>')
-print(f'Filtering out {unfamilliar_skill}')
 
-#def find_jobs():
-html_text = requests.get("https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=").text
-soup = BeautifulSoup(html_text, 'lxml')
-jobs= soup.find_all('li', class_="clearfix job-bx wht-shd-bx")
-for job in jobs:
-    published_date = job.find('span', class_='sim-posted').span.text
-    if 'few' in published_date:
-        company_name=job.find('h3',class_ ='joblist-comp-name').text.replace(' ','')
-        skills=job.find('span',class_ ='srp-skills').text.replace(' ','')
-        more_info=job.header.h2.a['href']
-        if unfamilliar_skill not in skills:
-            print(f"Company Name :{company_name.strip()}")
-            print(f"Required Skill:{skills.strip()}")
-            print(f"More Info : {more_info}")
+def scrape_jobs(url):
+    html_text = requests.get(url).text
+    soup = BeautifulSoup(html_text, 'html.parser')
+    jobs = soup.find_all('li', class_="clearfix job-bx wht-shd-bx")
+
+    for job in jobs:
+        published_date = job.find('span', class_='sim-posted').span.text
+        if 'few' in published_date:
+            company_name = job.find('h3', class_='joblist-comp-name').text.replace(' ', '')
+            skills = job.find('span', class_='srp-skills').text.replace(' ', '')
+            more_info = job.header.h2.a['href']
+            salary_element = job.find('span', class_='salary')
+            if salary_element is not None:
+                salary = salary_element.text.strip()
+            else:
+                salary = 'Not specified'
+            print(f"Company name: {company_name.strip()}")
+            print(f"Required skill: {skills.strip()}")
+            print(f"Salary: {salary}")
+            print(f"More info: {more_info}")
             print('')
+
+print("Enter a job you're interested in:")
+search = input(">")
+
+# Scraping from TimesJobs
+timesjobs_url = f"https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords={search}&txtLocation="
+print("TimesJobs:")
+scrape_jobs(timesjobs_url)
+
+# Scraping from FlexJobs
+flexjobs_url = "https://www.flexjobs.com/?sub=1967&utm_source=hasoffers&utm_medium=affiliate&utm_campaign=Dotdash"
+print("\nFlexJobs:")
+scrape_jobs(flexjobs_url)
+
+
+
+
+
+
+
+
