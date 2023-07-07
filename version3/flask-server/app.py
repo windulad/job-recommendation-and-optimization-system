ec78f1bdbd95198fc713e61d8b7b37e4d0956089
Windula Dissanayake <charithwinwindula@gmail.com>
2023-07-07 23:44:38 +0530

    commmit

--- version3/flask-server/app.py
+++ version3/flask-server/app.py
@@ -196,78 +196,76 @@ def enter_cv():
             'session_value': session.get('session_value')
         }
 
-# Enter Manual --------------------------------
-@app.route('/enter_manual', methods=['POST','GET'])
-def enter_manual():
+# # Enter Manual --------------------------------
+# @app.route('/enter_manual', methods=['POST','GET'])
+# def enter_manual():
     
-    user_id = session['user_id']
+#     user_id = session['user_id']
 
-    # Upload and Validate
-    if request.method == 'POST':
-        # Return skills extracted from html
-        user_skills = request.form.getlist('enter_manual_checkbox')
+#     # Upload and Validate
+#     if request.method == 'POST':
+#         # Return skills extracted from html
+#         user_skills = request.form.getlist('enter_manual_checkbox')
 
-        # Return other data
-        user_positions,user_miss_skills = manualread.ManualRead(user_skills)
+#         # Return other data
+#         user_positions,user_miss_skills = manualread.ManualRead(user_skills)
 
-        #Get element count
-        size_user_skills = len(user_skills)
-        size_user_positions = len(user_positions)
-        size_user_miss_skills = len(user_miss_skills)
+#         #Get element count
+#         size_user_skills = len(user_skills)
+#         size_user_positions = len(user_positions)
+#         size_user_miss_skills = len(user_miss_skills)
 
-        print('userid:',user_id)
-        print('skills count:',size_user_skills,'skills:',user_skills)
-        print('position count:',size_user_positions,'positions:',user_positions)
-        print('miss skill count:',size_user_miss_skills,'miss skills:',user_miss_skills)
+#         print('userid:',user_id)
+#         print('skills count:',size_user_skills,'skills:',user_skills)
+#         print('position count:',size_user_positions,'positions:',user_positions)
+#         print('miss skill count:',size_user_miss_skills,'miss skills:',user_miss_skills)
 
-        #Prepare for database input
-        for skill in range(size_user_skills,50):
-            user_skills.append('')
+#         #Prepare for database input
+#         for skill in range(size_user_skills,50):
+#             user_skills.append('')
 
-        for position in range(size_user_positions,5):
-            user_positions.append('')
+#         for position in range(size_user_positions,5):
+#             user_positions.append('')
 
-        for position in range(size_user_miss_skills,50):
-            user_miss_skills.append('')
+#         for position in range(size_user_miss_skills,50):
+#             user_miss_skills.append('')
 
-        # Unpack skills from user_skills list
-        skill_1,skill_2,skill_3,skill_4,skill_5,skill_6,skill_7,skill_8,skill_9,skill_10,skill_11,skill_12,skill_13,skill_14,skill_15,skill_16,skill_17,skill_18,skill_19,skill_20,skill_21,skill_22,skill_23,skill_24,skill_25,skill_26,skill_27,skill_28,skill_29,skill_30,skill_31,skill_32,skill_33,skill_34,skill_35,skill_36,skill_37,skill_38,skill_39,skill_40,skill_41,skill_42,skill_43,skill_44,skill_45,skill_46,skill_47,skill_48,skill_49,skill_50 = user_skills
+#         # Unpack skills from user_skills list
+#         skill_1,skill_2,skill_3,skill_4,skill_5,skill_6,skill_7,skill_8,skill_9,skill_10,skill_11,skill_12,skill_13,skill_14,skill_15,skill_16,skill_17,skill_18,skill_19,skill_20,skill_21,skill_22,skill_23,skill_24,skill_25,skill_26,skill_27,skill_28,skill_29,skill_30,skill_31,skill_32,skill_33,skill_34,skill_35,skill_36,skill_37,skill_38,skill_39,skill_40,skill_41,skill_42,skill_43,skill_44,skill_45,skill_46,skill_47,skill_48,skill_49,skill_50 = user_skills
 
-        #Unpack positions from user_positions list
-        position_1,position_2,position_3,position_4,position_5 = user_positions
+#         #Unpack positions from user_positions list
+#         position_1,position_2,position_3,position_4,position_5 = user_positions
 
-        #Unpack missing skills from user_miss_skills list
-        miss_1,miss_2,miss_3,miss_4,miss_5,miss_6,miss_7,miss_8,miss_9,miss_10,miss_11,miss_12,miss_13,miss_14,miss_15,miss_16,miss_17,miss_18,miss_19,miss_20,miss_21,miss_22,miss_23,miss_24,miss_25,miss_26,miss_27,miss_28,miss_29,miss_30,miss_31,miss_32,miss_33,miss_34,miss_35,miss_36,miss_37,miss_38,miss_39,miss_40,miss_41,miss_42,miss_43,miss_44,miss_45,miss_46,miss_47,miss_48,miss_49,miss_50 = user_miss_skills
+#         #Unpack missing skills from user_miss_skills list
+#         miss_1,miss_2,miss_3,miss_4,miss_5,miss_6,miss_7,miss_8,miss_9,miss_10,miss_11,miss_12,miss_13,miss_14,miss_15,miss_16,miss_17,miss_18,miss_19,miss_20,miss_21,miss_22,miss_23,miss_24,miss_25,miss_26,miss_27,miss_28,miss_29,miss_30,miss_31,miss_32,miss_33,miss_34,miss_35,miss_36,miss_37,miss_38,miss_39,miss_40,miss_41,miss_42,miss_43,miss_44,miss_45,miss_46,miss_47,miss_48,miss_49,miss_50 = user_miss_skills
 
-        # Connect to 'user_data.db' database
-        connection = sqlite3.connect(DATABASE)
-        cursor = connection.cursor()
+#         # Connect to 'user_data.db' database
+#         connection = sqlite3.connect(DATABASE)
+#         cursor = connection.cursor()
 
-        # Insert the data into the table skills
-        cursor.execute("INSERT INTO skills (userid,skillcount) VALUES (?,?)",(user_id,size_user_skills))
-        cursor.execute("UPDATE skills SET skill_1=?,skill_2=?,skill_3=?,skill_4=?,skill_5=?,skill_6=?,skill_7=?,skill_8=?,skill_9=?,skill_10=?,skill_11=?,skill_12=?,skill_13=?,skill_14=?,skill_15=?,skill_16=?,skill_17=?,skill_18=?,skill_19=?,skill_20=?,skill_21=?,skill_22=?,skill_23=?,skill_24=?,skill_25=?,skill_26=?,skill_27=?,skill_28=?,skill_29=?,skill_30=?,skill_31=?,skill_32=?,skill_33=?,skill_34=?,skill_35=?,skill_36=?,skill_37=?,skill_38=?,skill_39=?,skill_40=?,skill_41=?,skill_42=?,skill_43=?,skill_44=?,skill_45=?,skill_46=?,skill_47=?,skill_48=?,skill_49=?,skill_50=? WHERE userid=?",(skill_1,skill_2,skill_3,skill_4,skill_5,skill_6,skill_7,skill_8,skill_9,skill_10,skill_11,skill_12,skill_13,skill_14,skill_15,skill_16,skill_17,skill_18,skill_19,skill_20,skill_21,skill_22,skill_23,skill_24,skill_25,skill_26,skill_27,skill_28,skill_29,skill_30,skill_31,skill_32,skill_33,skill_34,skill_35,skill_36,skill_37,skill_38,skill_39,skill_40,skill_41,skill_42,skill_43,skill_44,skill_45,skill_46,skill_47,skill_48,skill_49,skill_50,user_id))
+#         # Insert the data into the table skills
+#         cursor.execute("INSERT INTO skills (userid,skillcount) VALUES (?,?)",(user_id,size_user_skills))
+#         cursor.execute("UPDATE skills SET skill_1=?,skill_2=?,skill_3=?,skill_4=?,skill_5=?,skill_6=?,skill_7=?,skill_8=?,skill_9=?,skill_10=?,skill_11=?,skill_12=?,skill_13=?,skill_14=?,skill_15=?,skill_16=?,skill_17=?,skill_18=?,skill_19=?,skill_20=?,skill_21=?,skill_22=?,skill_23=?,skill_24=?,skill_25=?,skill_26=?,skill_27=?,skill_28=?,skill_29=?,skill_30=?,skill_31=?,skill_32=?,skill_33=?,skill_34=?,skill_35=?,skill_36=?,skill_37=?,skill_38=?,skill_39=?,skill_40=?,skill_41=?,skill_42=?,skill_43=?,skill_44=?,skill_45=?,skill_46=?,skill_47=?,skill_48=?,skill_49=?,skill_50=? WHERE userid=?",(skill_1,skill_2,skill_3,skill_4,skill_5,skill_6,skill_7,skill_8,skill_9,skill_10,skill_11,skill_12,skill_13,skill_14,skill_15,skill_16,skill_17,skill_18,skill_19,skill_20,skill_21,skill_22,skill_23,skill_24,skill_25,skill_26,skill_27,skill_28,skill_29,skill_30,skill_31,skill_32,skill_33,skill_34,skill_35,skill_36,skill_37,skill_38,skill_39,skill_40,skill_41,skill_42,skill_43,skill_44,skill_45,skill_46,skill_47,skill_48,skill_49,skill_50,user_id))
 
-        # Insert the data into the table skills
-        cursor.execute("INSERT INTO positions (userid,positioncount) VALUES (?,?)",(user_id,size_user_positions))
-        cursor.execute("UPDATE positions SET position_1=?,position_2=?,position_3=?,position_4=?,position_5=? WHERE userid=?",(position_1,position_2,position_3,position_4,position_5,user_id))
+#         # Insert the data into the table skills
+#         cursor.execute("INSERT INTO positions (userid,positioncount) VALUES (?,?)",(user_id,size_user_positions))
+#         cursor.execute("UPDATE positions SET position_1=?,position_2=?,position_3=?,position_4=?,position_5=? WHERE userid=?",(position_1,position_2,position_3,position_4,position_5,user_id))
 
-        # Insert the data into the table skills
-        cursor.execute("INSERT INTO miss (userid,missskillcount) VALUES (?,?)",(user_id,size_user_miss_skills))
-        cursor.execute("UPDATE miss SET miss_1=?,miss_2=?,miss_3=?,miss_4=?,miss_5=?,miss_6=?,miss_7=?,miss_8=?,miss_9=?,miss_10=?,miss_11=?,miss_12=?,miss_13=?,miss_14=?,miss_15=?,miss_16=?,miss_17=?,miss_18=?,miss_19=?,miss_20=?,miss_21=?,miss_22=?,miss_23=?,miss_24=?,miss_25=?,miss_26=?,miss_27=?,miss_28=?,miss_29=?,miss_30=?,miss_31=?,miss_32=?,miss_33=?,miss_34=?,miss_35=?,miss_36=?,miss_37=?,miss_38=?,miss_39=?,miss_40=?,miss_41=?,miss_42=?,miss_43=?,miss_44=?,miss_45=?,miss_46=?,miss_47=?,miss_48=?,miss_49=?,miss_50=? WHERE userid=?",(miss_1,miss_2,miss_3,miss_4,miss_5,miss_6,miss_7,miss_8,miss_9,miss_10,miss_11,miss_12,miss_13,miss_14,miss_15,miss_16,miss_17,miss_18,miss_19,miss_20,miss_21,miss_22,miss_23,miss_24,miss_25,miss_26,miss_27,miss_28,miss_29,miss_30,miss_31,miss_32,miss_33,miss_34,miss_35,miss_36,miss_37,miss_38,miss_39,miss_40,miss_41,miss_42,miss_43,miss_44,miss_45,miss_46,miss_47,miss_48,miss_49,miss_50,user_id))
+#         # Insert the data into the table skills
+#         cursor.execute("INSERT INTO miss (userid,missskillcount) VALUES (?,?)",(user_id,size_user_miss_skills))
+#         cursor.execute("UPDATE miss SET miss_1=?,miss_2=?,miss_3=?,miss_4=?,miss_5=?,miss_6=?,miss_7=?,miss_8=?,miss_9=?,miss_10=?,miss_11=?,miss_12=?,miss_13=?,miss_14=?,miss_15=?,miss_16=?,miss_17=?,miss_18=?,miss_19=?,miss_20=?,miss_21=?,miss_22=?,miss_23=?,miss_24=?,miss_25=?,miss_26=?,miss_27=?,miss_28=?,miss_29=?,miss_30=?,miss_31=?,miss_32=?,miss_33=?,miss_34=?,miss_35=?,miss_36=?,miss_37=?,miss_38=?,miss_39=?,miss_40=?,miss_41=?,miss_42=?,miss_43=?,miss_44=?,miss_45=?,miss_46=?,miss_47=?,miss_48=?,miss_49=?,miss_50=? WHERE userid=?",(miss_1,miss_2,miss_3,miss_4,miss_5,miss_6,miss_7,miss_8,miss_9,miss_10,miss_11,miss_12,miss_13,miss_14,miss_15,miss_16,miss_17,miss_18,miss_19,miss_20,miss_21,miss_22,miss_23,miss_24,miss_25,miss_26,miss_27,miss_28,miss_29,miss_30,miss_31,miss_32,miss_33,miss_34,miss_35,miss_36,miss_37,miss_38,miss_39,miss_40,miss_41,miss_42,miss_43,miss_44,miss_45,miss_46,miss_47,miss_48,miss_49,miss_50,user_id))
         
-        # Disconnect from 'user_data.db' database
-        connection.commit()
+#         # Disconnect from 'user_data.db' database
+#         connection.commit()
 
-        return redirect(url_for('homepage'))
+#         return redirect(url_for('homepage'))
 
-    else:
-        return render_template("enter_manual.html")
-
-
-
+#     else:
+#         return render_template("enter_manual.html")
 
 # Homepage --------------------------------------
 @app.route('/homepage', methods=['POST','GET'])
+@cross_origin(supports_credentials=True)
 def homepage():
     
     user_id = session['user_id']
--- version3/react-client/src/Components/Home.js
+++ version3/react-client/src/Components/Home.js
@@ -1,58 +1,30 @@
 import React, { useEffect, useState } from "react";
 import Axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 const SERVER_URL = 'http://127.0.0.1:5000';
 
 function Home(){
-    const [responseData, setResponseData] = useState(null);
     const navigate = useNavigate();
 
-    const [details, setDetails] = useState({
-        email: "",
-        password: ""
-    });
+    // GET session_value from 'Entercv.js' OR 'Entermanual.js'
+    const location = useLocation();
+    const { state } = location;
+    const session_value = state.user_id;
+    console.log(session_value)
 
-    
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    var json_details = JSON.stringify(session_value);
 
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setDetails((prev) => {
-            return {...prev, [name]: value}
-        })
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        var json_details = JSON.stringify(details);
-
-        // Make the POST request to 'ENDPOINT1' using Axios
-        Axios.post(SERVER_URL+'/login', json_details)
-        .then(response => {
-            //console.log(json_details);
-            const message = response.data.message;
-            console.log(message)
-
-            const user_id = response.data.user_id;
-            console.log(user_id)
-
-            // Store user_id in browser storage
-            localStorage.setItem('user_id', user_id);
-
-            setResponseData(response.data);
-
-            if (message === 'error-2'){
-                navigate('/login');
-            }else if(message === 'success-2'){
-                navigate('/home');
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await Axios.post(SERVER_URL+'/homepage', json_details, { withCredentials: true });
+                console.log(response.data);
+            } catch (error) {
+                console.error(error);
             }
-        })
-        .catch(error => {
-            console.error(error);
-        });
-    }
+        };
+        fetchData();
+    }, []);
 
 
     const handleclick1 = () => {
@@ -91,25 +63,25 @@ function Home(){
                     <li style={{float:"right"}}><a onClick={handleclick6}>Log out</a></li>
                 </ul>
             </div>
-
-            <div class="main">
-                <div class="search">
-                    <h1 class="main_title">Latest Courses to Follow</h1>
-                    <form onSubmit={handleSubmit}>
-                        <span class="search_field">
-                            <input type="text" name="position" placeholder="Position"/>
-                        </span>
-                        <span class="search_field">
-                            <input type="text" name="location" placeholder="Location"/>
-                        </span>
-                        <span class="search_field">
-                            <input type="submit" value="Search"/>
-                        </span>
-                    </form>
-                </div>
-            </div>
         </>
     )
 }
 
 export default Home;
+
+/* <div class="main">
+    <div class="search">
+        <h1 class="main_title">Latest Courses to Follow</h1>
+        <form onSubmit={handleSubmit}>
+            <span class="search_field">
+                <input type="text" name="position" placeholder="Position"/>
+            </span>
+            <span class="search_field">
+                <input type="text" name="location" placeholder="Location"/>
+            </span>
+            <span class="search_field">
+                <input type="submit" value="Search"/>
+            </span>
+        </form>
+    </div>
+</div> */
