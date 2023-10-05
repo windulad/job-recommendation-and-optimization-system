e508ac159720cf3e04d89b60d05b7dc48eb16fa0
Windula Dissanayake <charithwinwindula@gmail.com>
2023-10-05 11:12:53 +0530

    commit

--- version4/react-client/src/Components/Profile.js
+++ version4/react-client/src/Components/Profile.js
@@ -168,7 +168,7 @@ function Home(){
         <div>
         <div>
             <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="">Navbar</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
